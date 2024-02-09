366a94104e247cdf0ee30e9a0a9702d7f71a8336
Vikram <vikram.sh86@gmail.com>
2024-02-09 21:41:07 +0100

    Initial Commit

---
+++ Rock-Paper-Lizard/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rock-Paper-Lizard
+//
+//  Created by Vikram on 09/02/2024.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Rock-Paper-Lizard/Rock_Paper_LizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_LizardApp.swift
+//  Rock-Paper-Lizard
+//
+//  Created by Vikram on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_LizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
